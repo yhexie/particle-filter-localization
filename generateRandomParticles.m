02522d91fc518fa4a884122342b551c2f368a942
achamber <achamber@gmail.com>
2011-10-10 22:12:29 +0000

    Function to add random particles in free space

---
+++ generateRandomParticles.m
@@ -0,0 +1,16 @@
+function [ particle_mat ] = generateRandomParticles( numParticles, global_map, map_resolution, occupied_threshold )
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+% Find all free space on the map
+[freeCellsX, freeCellsY] = find(global_map > occupied_threshold);
+freeCellIndices = randperm(length(freeCellsX));
+
+% Randomly place particles in the free space
+particle_mat = [    map_resolution*freeCellsX(freeCellIndices(1:numParticles))';
+    map_resolution*freeCellsY(freeCellIndices(1:numParticles))';
+    2*pi*rand(1, numParticles)];
+
+
+end
+
