b5192f3117c32888e55d2ce2b08002e8c53d7cc5
achamber <achamber@gmail.com>
2011-10-11 11:31:50 +0000

    Switched to using Sankalp's ray casting function since it is MUCH faster!

--- beam_range_finder_model.m
+++ beam_range_finder_model.m
@@ -29,17 +29,20 @@ robot_position_m = xt(1:2);
 laser_position_m = robot_position_m + [ 0.25*cosd(robot_angle_deg); 
                                         0.25*sind(robot_angle_deg)];
 
+z_expected = zeros(1,numLaserScans);
+z_expected = findExpectedRange_(robot_angle_deg, laser_position_m, map, laser_max_range, occupied_threshold, map_resolution);
+
 for k = 1:30:numLaserScans
     
-    laser_angle_deg = robot_angle_deg - 90 + (k - 0.5);
+%     laser_angle_deg = robot_angle_deg - 90 + (k - 0.5);
     % Ray cast to find expected range
     
-    z_expected = findExpectedRange(laser_angle_deg, laser_position_m, map, laser_max_range, occupied_threshold, map_resolution);
+%     z_expected(k) = findExpectedRange(laser_angle_deg, laser_position_m, map, laser_max_range, occupied_threshold, map_resolution);
     
-    pHit = calcProbHit(zt(k), z_expected, laser_max_range, std_dev_hit);
-    pShort = calcProbShort(zt(k), z_expected, laser_max_range, lambda_short);
-    pMax = calcProbMax(zt(k), z_expected, laser_max_range);
-    pRand = calcProbRand(zt(k), z_expected, laser_max_range);
+    pHit = calcProbHit(zt(k), z_expected(k), laser_max_range, std_dev_hit);
+    pShort = calcProbShort(zt(k), z_expected(k), laser_max_range, lambda_short);
+    pMax = calcProbMax(zt(k), z_expected(k), laser_max_range);
+    pRand = calcProbRand(zt(k), z_expected(k), laser_max_range);
     
     p = zHit*pHit + zShort*pShort + zMax*pMax + zRand*pRand;
     
@@ -47,5 +50,6 @@ for k = 1:30:numLaserScans
 end
 
 
+% figure, plot(z_expected, '.');
 end
 
